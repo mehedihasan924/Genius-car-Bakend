175fb2cc8dfdad4c0ad61a6ed9378331b9d9af2e
mehedihasan924 <mdhasanmehedi924@gmail.com>
2024-05-02 01:07:41 +0600

    added

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollecttion = client.db('geniusCar').collection('service');
+    const orderCollection = client.db('geniusCar').collection('orders');
+
 
     app.get('/services', async (req, res) => {
       const query = {}
@@ -38,14 +40,30 @@ async function run() {
       res.send(srevices);
     });
 
-    app.get('/services/:id', async(req, res) =>{
-      const id=req.params.id;
-      const query = { _id: ObjectId(id) };
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const service = await serviceCollecttion.findOne(query);
-      res.send(service)    
+      res.send(service)
+    });
+
+    //orders API
+    app.get('/orders', async(req, res) => {
+      let query = {};
+         if (req.query.email){
+          query = {
+             email: req.query.email
+           }
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
     })
-
-
+       app.post('/orders', async(req, res) => {
+         const order = req.body;
+         const result = await orderCollection.insertOne(order);
+         res.send(result);
+    })
   }
   finally {
     
